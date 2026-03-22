3df828cd569bbe2875ff41e28250d88b276a6562
SPARTAN <gigi.onyegbula.2007@gmail.com>
2026-03-22 12:45:02 +0100

    Initialized Project

---
+++ src/main/java/com/example/Student/Manage/System/StudentManageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Student.Manage.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManageSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Student/Manage/System/StudentManageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Student.Manage.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManageSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
